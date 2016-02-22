ba16cd32d51e7f3445f43e0fe7f3ab6d509bb521
Brandon Arbuthnot <brandonarbuthnot7@gmail.com>
2016-02-22 18:40:56 -0500

    Update DriveSystem.cpp

--- DriveSystem.cpp
+++ DriveSystem.cpp
@@ -1,1 +1,24 @@
+#include "DriveSystem.h"
 
+DriveSystem* drive;
+
+DriveSystem::DriveSystem()
+{
+  //Initialize stuff
+  leftDrive = new FEHMotor(FEHMotor::Motor1);
+  rightDrive = new FEHMotor(FEHMotor::Motor0);
+  
+  //Variables that must be initialized before running the robot
+}
+
+void DriveSystem::MoveForward(float percent)
+{
+  leftDrive.SetPercent(percent);
+  rightDrive.SetPercent(percent);
+}
+
+void DriveSystem::MoveBackwards(float percent)
+{
+  leftDrive.SetPercent(-1*percent);
+  rightDrive.SetPercent(-1*percent);
+}
