b5939071d12fa7570e9f06a36c326cdf694f4271
fishbein16 <fishbein.16@osu.edu>
2016-02-22 20:25:24 -0500

    Update DriveSystem.cpp

--- DriveSystem.cpp
+++ DriveSystem.cpp
@@ -5,8 +5,8 @@ DriveSystem* drive;
 DriveSystem::DriveSystem()
 {
   //Initialize stuff
-  leftDrive = new FEHMotor(FEHMotor::Motor1);
-  rightDrive = new FEHMotor(FEHMotor::Motor0);
+  leftDrive = new FEHMotor(FEHMotor::Motor1, 12.0);
+  rightDrive = new FEHMotor(FEHMotor::Motor0, 12.0);
   
   //Variables that must be initialized before running the robot
 }
