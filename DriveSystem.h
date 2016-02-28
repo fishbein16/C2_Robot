9a27db4238f1d6c443d826f5c9108c3c000d3667
Brandon Arbuthnot <brandonarbuthnot7@gmail.com>
2016-02-28 13:22:23 -0500

    Update DriveSystem.h

--- DriveSystem.h
+++ DriveSystem.h
@@ -1,71 +1,33 @@
-#include "DriveSystem.h"
+#ifndef DRIVESYSTEM_H
+#define DRIVESYSTEM_H
 
-DriveSystem* drive = new DriveSystem();
+#include <FEHMotor.h>
+#include <FEHIO.h>
 
-DriveSystem::DriveSystem()
+class DriveSystem
 {
-  //Initialize stuff
-  rightDrive = new FEHMotor(FEHMotor::Motor0, 12.0);
-  leftDrive = new FEHMotor(FEHMotor::Motor1, 12.0);
-  
-  rightEncoder = new DigitalEncoder(FEHIO::P1_0);
-  leftEncoder = new DigitalEncoder(FEHIO::P1_1);
-  
-  //Variables that must be initialized before running the robot
-}
+public:
+    DriveSystem();
+    int GetLeftEncoderCounts();
+    int GetRightEncoderCounts();
+    void ResetEncoders();
+    void MoveForward(float percent);
+    void MoveBackwards(float percent);
+    void ZeroTurnRight(float angle);
+    void ZeroTurnLeft(float angle);
+    void ForwardTurn(float rightPercent, float leftPercent);
+    void BackwardsTurn(float rightPercent, float leftPercent);
+    void Stop();
 
-int DriveSystem::GetLeftEncoderCounts()
-{
-    return leftEncoder->Counts();
-}
+private:
+    FEHMotor* leftDrive;
+    FEHMotor* rightDrive;
 
-int DriveSystem::GetRightEncoderCounts()
-{
-    return rightEncoder->Counts();
-}
+    DigitalEncoder* rightEncoder;
+    DigitalEncoder* leftEncoder;
+};
 
-void DriveSystem::ResetEncoders()
-{
-    leftEncoder->ResetCounts();
-    rightEncoder->ResetCounts();
-}
+//Making object pointer global
+extern DriveSystem* drive;
 
-void DriveSystem::MoveForward(float percent)
-{
-  leftDrive->SetPercent(-1 * percent);
-  rightDrive->SetPercent(-1 * percent);
-}
-
-void DriveSystem::MoveBackwards(float percent)
-{
-  leftDrive->SetPercent(percent);
-  rightDrive->SetPercent(percent);
-}
-
-void DriveSystem::ZeroTurnRight(float angle)
-{
-  //Encoder calculations here
-}
-
-void DriveSystem::ZeroTurnLeft(float angle)
-{
-  //Encoder calculations here
-}
-
-void DriveSystem::ForwardTurn(float rightPercent, float leftPercent)
-{
-  rightDrive->SetPercent(-1 * rightPercent);
-  leftDrive->SetPercent(-1 * leftPercent);
-}
-
-void DriveSystem::BackwardsTurn(float rightPercent, float leftPercent)
-{
-  rightDrive->SetPercent(rightPercent);
-  leftDrive->SetPercent(leftPercent);
-}
-
-void DriveSystem::Stop()
-{
-  rightDrive->Stop();
-  leftDrive->Stop();
-}
+#endif // DRIVESYSTEM_H
