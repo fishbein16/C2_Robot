1aa19b0e1c79d756829577c953ceba4c43198be4
fishbein16 <fishbein.16@osu.edu>
2016-03-08 11:46:00 -0500

    Update DriveSystem.cpp

--- DriveSystem.cpp
+++ DriveSystem.cpp
@@ -11,20 +11,20 @@ DriveSystem::DriveSystem()
   rightEncoder = new DigitalInputPin(FEHIO::P1_0);
   leftEncoder = new DigitalInputPin(FEHIO::P1_1);
 
-  leftSensor = new AnalogInputPin(FEHIO::P0_0);
+  leftSensor = new AnalogInputPin(FEHIO::P0_2);
   middleSensor = new AnalogInputPin(FEHIO::P0_1);
-  rightSensor = new AnalogInputPin(FEHIO::P0_2);
+  rightSensor = new AnalogInputPin(FEHIO::P0_0);
 
   lineState = STATE_IDLE;
 }
 
-void DriveSystem::WaitForSetpoint(int setpoint)
+void DriveSystem::WaitForSetpointAngle(int setpoint)
 {
   int leftCounts = 0;
   int rightCounts = 0;
   bool oldLeftValue = leftEncoder->Value();
   bool oldRightValue = rightEncoder->Value();
-  while(rightCounts < setpoint)
+  while((leftCounts + rightCounts) / 2 < setpoint)
   {
     LCD.Write("Left Encoder: ");
     LCD.WriteLine(leftCounts);
@@ -48,16 +48,49 @@ void DriveSystem::WaitForSetpoint(int setpoint)
   }
 }
 
+void DriveSystem::WaitForSetpointInch(double setpoint)
+{
+    int leftCounts = 0;
+    int rightCounts = 0;
+    bool oldLeftValue = leftEncoder->Value();
+    bool oldRightValue = rightEncoder->Value();
+
+    int encoderSetpoint = setpoint * INCH_COUNT_CONVERSION;
+
+    while((leftCounts + rightCounts) / 2 < encoderSetpoint)
+    {
+      LCD.Write("Left Encoder: ");
+      LCD.WriteLine(leftCounts);
+
+      LCD.Write("RightEncoder: ");
+      LCD.WriteLine(rightCounts);
+
+      bool newLeftValue = leftEncoder->Value();
+      if(newLeftValue != oldLeftValue)
+      {
+          leftCounts++;
+          oldLeftValue = newLeftValue;
+      }
+
+      bool newRightValue = rightEncoder->Value();
+      if(newRightValue != oldRightValue)
+      {
+        rightCounts++;
+        oldRightValue = newRightValue;
+      }
+    }
+}
+
 void DriveSystem::MoveForward(float percent)
 {
   leftDrive->SetPercent(-1 * percent);
-  rightDrive->SetPercent(-1 * percent);
+  rightDrive->SetPercent(-0.9 * percent);
 }
 
 void DriveSystem::MoveBackwards(float percent)
 {
   leftDrive->SetPercent(percent);
-  rightDrive->SetPercent(percent);
+  rightDrive->SetPercent(percent * 0.9);
 }
 
 void DriveSystem::ZeroTurnClockwise(float angle)
@@ -65,7 +98,7 @@ void DriveSystem::ZeroTurnClockwise(float angle)
   int counts = angle/90 * 48;
   leftDrive->SetPercent(-1*30.0);
   rightDrive->SetPercent(30.0);
-  WaitForSetpoint(counts);
+  WaitForSetpointAngle(counts);
   rightDrive->Stop();
   leftDrive->Stop();
 }
@@ -75,127 +108,115 @@ void DriveSystem::ZeroTurnCounter(float angle)
     int counts = angle/90 * 48;
     leftDrive->SetPercent(30.0);
     rightDrive->SetPercent(-1*30.0);
-    WaitForSetpoint(counts);
+    WaitForSetpointAngle(counts);
     rightDrive->Stop();
     leftDrive->Stop();
 
 }
 
-void DriveSystem::ForwardTurn(float rightPercent, float leftPercent)
+void DriveSystem::ForwardTurn(float leftPercent, float rightPercent)
 {
-  rightDrive->SetPercent(-1 * rightPercent);
   leftDrive->SetPercent(-1 * leftPercent);
+  rightDrive->SetPercent(-1 * rightPercent);
 }
 
-void DriveSystem::BackwardsTurn(float rightPercent, float leftPercent)
+void DriveSystem::BackwardsTurn(float leftPercent, float rightPercent)
 {
-  rightDrive->SetPercent(rightPercent);
   leftDrive->SetPercent(leftPercent);
+  rightDrive->SetPercent(rightPercent);
 }
 
 void DriveSystem::Stop()
 {
-  rightDrive->Stop();
   leftDrive->Stop();
+  rightDrive->Stop();
 }
 
-void DriveSystem::LineFollowingFunc(int target)
+void DriveSystem::LineFollowingFunc()
 {
-    int leftCount = 0;
-    int rightCount = 0;
-    bool oldLeftValue;
-    bool oldRightValue;
-
     int majorCount = 0;
 
     LineFollowing lastLineState = STATE_IDLE;
 
-    while(true) //Change later for some sort of condition
+    while(majorCount < 4) //Change later for some sort of condition
     {
-        leftSeen = leftSensor->Value() > LEFT_THRESHOLD;
-        rightSeen = rightSensor->Value() > MID_THRESHOLD;
-        midSeen = middleSensor->Value() > RIGHT_THRESHOLD;
+        leftSeen = leftSensor->Value() < LEFT_THRESHOLD;
+        rightSeen = rightSensor->Value() < RIGHT_THRESHOLD;
+        midSeen = middleSensor->Value() < MID_THRESHOLD;
 
-        if(midSeen && (!leftSeen && !rightSeen)) {
+        if(midSeen && (!leftSeen && !rightSeen))
+        {
             lineState = STATE_STRAIGHT;
         }
-        else if((midSeen && rightSeen) &&!leftSeen) {
+        else if((midSeen && rightSeen) &&!leftSeen)
+        {
             lineState = STATE_MINOR_RIGHT;
         }
-        else if ((midSeen && leftSeen) && !rightSeen) {
+        else if ((midSeen && leftSeen) && !rightSeen)
+        {
             lineState = STATE_MINOR_LEFT;
         }
-        else if (leftSeen && (!rightSeen && !midSeen)) {
+        else if (leftSeen && (!rightSeen && !midSeen))
+        {
             lineState = STATE_MAJOR_LEFT;
         }
-        else if (rightSeen && (!leftSeen && !midSeen)) {
+        else if (rightSeen && (!leftSeen && !midSeen))
+        {
             lineState = STATE_MAJOR_RIGHT;
         }
 
-        switch(lineState) {
+        switch(lineState)
+        {
         case STATE_STRAIGHT:
-            drive->ForwardTurn(FULL_PERCENT, FULL_PERCENT);
+            MoveForward(FULL_PERCENT);
             LCD.WriteLine("STATE_STRAIGHT");
             break;
         case STATE_MINOR_LEFT:
-            drive->ForwardTurn(MINOR_PERCENT, FULL_PERCENT);
+            ForwardTurn(MINOR_PERCENT, FULL_PERCENT);
             LCD.WriteLine("STATE_MIL");
             break;
         case STATE_MINOR_RIGHT:
-            drive->ForwardTurn(FULL_PERCENT, MINOR_PERCENT);
+            ForwardTurn(FULL_PERCENT, MINOR_PERCENT);
             LCD.WriteLine("STATE_MIR");
             break;
         case STATE_MAJOR_LEFT:
-            drive->ForwardTurn(MAJOR_PERCENT, FULL_PERCENT);
+            ForwardTurn(MAJOR_PERCENT, FULL_PERCENT);
             LCD.WriteLine("STATE_MAL");
             break;
         case STATE_MAJOR_RIGHT:
-            drive->ForwardTurn(FULL_PERCENT, MAJOR_PERCENT);
+            ForwardTurn(FULL_PERCENT, MAJOR_PERCENT);
             LCD.WriteLine("STATE_MAR");
             if(lastLineState != lineState)
             {
                 majorCount++;
             }
-
-            if(majorCount == 2)
-            {
-                oldLeftValue = leftEncoder->Value();
-                oldRightValue = rightEncoder->Value();
-            }
             break;
         case STATE_IDLE:
-            drive->Stop();
+            Stop();
             LCD.WriteLine("STATE_IDLE");
             break;
         }
 
         lastLineState = lineState;
+    }
 
-        if(majorCount == 2)
+    Stop();
+}
+
+void DriveSystem::LineFollowingBack()
+{
+    bool startFollowing = false;
+    bool lastRight = false;
+
+    while(!startFollowing)
+    {
+        MoveBackwards(50);
+        rightSeen = rightSensor->Value() < RIGHT_THRESHOLD;
+        if(lastRight && !rightSeen)
         {
-            LCD.Write("Left Encoder: ");
-            LCD.WriteLine(leftCount);
-
-            LCD.Write("RightEncoder: ");
-            LCD.WriteLine(rightCount);
-
-            bool newLeftValue = leftEncoder->Value();
-            if(newLeftValue != oldLeftValue)
-            {
-                leftCount++;
-                oldLeftValue = newLeftValue;
-            }
-
-            bool newRightValue = rightEncoder->Value();
-            if(newRightValue != oldRightValue)
-            {
-              rightCount++;
-              oldRightValue = newRightValue;
-            }
+            startFollowing = true;
+            Stop();
         }
-        if((leftCount + rightCount) / 2.0 >= target)
-        {
-            break;
-        }
+        lastRight = rightSeen;
     }
 }
