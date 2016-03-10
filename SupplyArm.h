e81ffe140798b3acd25566ca82fcfbb7dd5cb0b7
fishbein16 <fishbein.16@osu.edu>
2016-03-10 12:12:48 -0500

    Create SupplyArm.h

---
+++ SupplyArm.h
@@ -0,0 +1,24 @@
+#ifndef SUPPLYARM_H
+#define SUPPLYARM_H
+
+#include <FEHServo.h>
+
+#define SUPPLY_SERVO_MIN    500
+#define SUPPLY_SERVO_MAX    2340
+
+#define PICK_UP             180.0
+#define DROP_OFF            5.0
+
+class SupplyArm
+{
+public:
+    SupplyArm();
+    void ToPickUp();
+    void ToDropOff();
+private:
+    FEHServo* supplyServo;
+};
+
+extern SupplyArm* supplyArm;
+
+#endif // SUPPLYARM_H
