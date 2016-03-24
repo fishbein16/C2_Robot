832450f44c849246db7f3652c68e583f19da1011
fishbein16 <fishbein.16@osu.edu>
2016-03-23 20:35:46 -0400

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -26,13 +26,134 @@ float Robot::StartValue()
 
 void Robot::FlipSwitches(int left, int middle, int right)
 {
-    //insert decision making code here
-    //call buttonSwitch->ToBlankSwitch()
+    bool againstWall = true;
+    if(left == 1)
+    {
+        buttonSwitch->ToLeftSwitch();
+        buttonSwitch->ArmOut();
+        Sleep(0.25);
+    }
+    else
+    {
+        buttonSwitch->ToLeftMidGap();
+        buttonSwitch->ArmOut();
+        Sleep(0.1);
+        buttonSwitch->ToLeftSwitch();
+
+        drive->MoveForward(20);
+        drive->WaitForSetpointInch(0.2);
+        drive->Stop();
+        Sleep(0.25);
+
+        againstWall = false;
+
+        buttonSwitch->ToLeftMidGap();
+    }
+
+    if(middle == 1)
+    {
+        if(againstWall)
+        {
+            drive->MoveForward(20);
+            drive->WaitForSetpointInch(0.2);
+            drive->Stop();
+            againstWall = false;
+            Sleep(0.25);
+        }
+        buttonSwitch->ToMiddleSwitch();
+
+        drive->MoveBackwards(20);
+        drive->WaitForSetpointInch(0.3);
+        drive->Stop();
+        Sleep(0.25);
+    }
+    else
+    {
+        if(!againstWall)
+        {
+            drive->MoveBackwards(20);
+            drive->WaitForSetpointInch(0.3);
+            drive->Stop();
+            againstWall = true;
+            Sleep(0.25);
+        }
+        buttonSwitch->ArmIn();
+        Sleep(0.1);
+        buttonSwitch->ToMiddleSwitch();
+        buttonSwitch->ArmOutMid();
+
+        drive->MoveForward(20);
+        drive->WaitForSetpointInch(0.2);
+        drive->Stop();
+        againstWall = false;
+        Sleep(0.25);
+    }
+
+    if(right == 1)
+    {
+        if(againstWall)
+        {
+            drive->MoveForward(20);
+            drive->WaitForSetpointInch(0.2);
+            drive->Stop();
+            Sleep(0.25);
+            againstWall = false;
+        }
+        buttonSwitch->ToRightSwitch();
+
+        drive->MoveBackwards(20);
+        drive->WaitForSetpointInch(0.3);
+        drive->Stop();
+        Sleep(0.25);
+        againstWall = true;
+    }
+    else
+    {
+        buttonSwitch->ToRightMidGap();
+
+        if(!againstWall)
+        {
+            drive->MoveBackwards(20);
+            drive->WaitForSetpointInch(0.3);
+            drive->Stop();
+            Sleep(0.25);
+            againstWall = true;
+        }
+        buttonSwitch->ToRightSwitch();
+
+        drive->MoveForward(20);
+        drive->WaitForSetpointInch(0.2);
+        drive->Stop();
+        Sleep(0.25);
+        againstWall = false;
+    }
+
+    if(!againstWall)
+    {
+        drive->MoveBackwards(20);
+        drive->WaitForSetpointInch(0.3);
+        drive->Stop();
+        Sleep(0.25);
+    }
+
+    buttonSwitch->ToRightMidGap();
+    buttonSwitch->ArmIn();
+    buttonSwitch->ToMiddleSwitch();
+}
+
+bool Robot::IsRed()
+{
+    return FuelValue() < RED_BLUE_THRESHOLD;
+}
+
+bool Robot::IsBlue()
+{
+    return FuelValue() < BLUE_NO_LIGHT_THRESHOLD && FuelValue() > RED_BLUE_THRESHOLD;
 }
 
 void Robot::PressButton()
 {
-    if(FuelValue() < RED_BLUE_THRESHOLD)
+    if(IsRed())
     {
         buttonSwitch->ToRedButton();
     }
@@ -41,6 +162,9 @@ void Robot::PressButton()
         buttonSwitch->ToBlueButton();
     }
 
-    //drive forward x counts
-    //do something to keep robot in place for five seconds
+    drive->MoveBackwards(20);
+
+    Sleep(5.25);
+
+    drive->Stop();
 }
