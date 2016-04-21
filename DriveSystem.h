94287a212754c55c3efb6cde61bfb539b8c69954
fishbein16 <fishbein.16@osu.edu>
2016-04-21 18:52:58 -0400

    Update DriveSystem.h

--- DriveSystem.h
+++ DriveSystem.h
@@ -43,7 +43,6 @@ public:
     void BackwardsTurn(float leftPercent, float righttPercent);
     void Stop();
     void LineFollowingFunc();
-    void LineFollowingBack();
 
 private:
     FEHMotor* leftDrive;
